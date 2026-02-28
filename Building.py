6e3a215eb7981fb148976863d4e3ded5c066cc50
Will McLean <wmclean@uvm.edu>
2026-02-28 10:26:55 -0500

    push

--- Building.py
+++ Building.py
@@ -1,16 +1,32 @@
 class Building:
     def __init__(self):
         self.level = 1
-        self.untilNextLevel = 10
+        self.untilNextLevel = 100
+        self.isDestroyed = False
 
-    def upgradeBuilding(self, gold):
-        if gold < self.untilNextLevel:
+    def upgradeBuilding(self, xp):
+        if xp < self.untilNextLevel:
             print("not enough to upgrade")
         else:
-            gold -= self.untilNextLevel
+            xp -= self.untilNextLevel
             self.level += 1
             self.untilNextLevel *= 2
             print("building upgraded!")
-        return gold
+        return xp
     
+    def getLevel(self):
+        return self.getLevel
     
+    def getUntilNextLevel(self):
+        return self.untilNextLevel
+    
+    def reBuild(self,xp):
+        if not self.isDestroyed:
+            print("Building isnt destroyed")
+        elif xp < 100:
+            print("Not enough to rebuild")
+        else:
+            self.isDestroyed = False
+            xp -= 100
+
+
